be2e22ffac7188e4ccee7bc2d060ffc14e8eae3f
luiscarvalho <l.rccarvalho@gmail.com>
2013-05-14 20:43:36 +0100

    sentiment analysis last commit

--- eadw/Extractandanalyze.py
+++ eadw/Extractandanalyze.py
@@ -3,11 +3,55 @@ import sys
 from collectionandstorage import Collection
 from extraction import Extractionpersonalities
 from search import Search
+from sentimentanalysis import SentimentAnalysis
+import pickle
+import string
+import re
 
-indexPath = '/Users/luiscarvalho/Desktop/EADW-Lab/project/indexdir/'
-persListpath = '/Users/luiscarvalho/Desktop/EADW-Lab/project/personalities.txt'
-stopWordspath = '/Users/luiscarvalho/Desktop/EADW-Lab/project/stopwords.txt'
+#------- Carrega o vocabulario com a polaridade -----------
+
+arq = open('SemA.txt','rb') 
+dicA = pickle.load(arq)
+arq.close()
+arq = open('SemB.txt','rb')
+dicB = pickle.load(arq)
+arq.close()
+arq = open('SemC.txt','rb')
+dicC = pickle.load(arq)
+arq.close()
+arq = open('SemD.txt','rb') 
+dicD = pickle.load(arq)
+arq.close()
+arq = open('SemE.txt','rb') 
+dicE = pickle.load(arq)
+arq.close()
+arq = open('SemF.txt','rb')
+dicF = pickle.load(arq)
+arq.close()
+
+vocabPolDict=dict()
+
+vocabPolDict.update(dicF)
+vocabPolDict.update(dicA)
+vocabPolDict.update(dicB)
+vocabPolDict.update(dicC)
+vocabPolDict.update(dicD)
+vocabPolDict.update(dicE)
+
+#print vocabPolDict
+
+indexPath = 'indexdir/'
+persListpath = 'personalities.txt'
 run = True
+
+#---------- carrega a lista de stopWord ---------
+stopwords=open("stopwords.txt","r")
+stopwordList=[]
+
+for line in stopwords:
+    l=line.split(",")
+    for word in l:
+        stopwordList.append(word)
 
 while(run):
     print "\n\nPortuguese political personalities\n"
@@ -15,18 +59,19 @@ while(run):
     print "2 - Stored News"
     print "3 - News Search"
     print "4 - Extraction of Named Entities"
-    print "5 - Sentiment Analysis"
-    print "6 - Exit\n"
+    print "5 - Sentiment Analysis for news"
+    print "6 - Sentiment Analysis for query"
+    print "7 - Exit\n"
     var = raw_input("Select an option: ")
     
-    if ("1" in var):
+    if "1" in var:
         c = Collection.CollectStores(indexPath)
         c.collectStores()
-    elif ("2" in var):
+    elif "2" in var:
         print "\n"
         search = Search.Searcher(indexPath)
         search.allFeeds()
-    elif ("3" in var):
+    elif "3" in var:
         print "\n"
         query = raw_input("Enter a query to search: ")
         search = Search.Searcher(indexPath)
@@ -35,7 +80,7 @@ while(run):
         for line in searchlist:
             print line
             print "\n"
-    elif ("4" in var):
+    elif "4" in var:
         print "\n"
         query = raw_input("Enter a query to search: ")
         extraction = Extractionpersonalities.Extraction(indexPath, persListpath)
@@ -49,9 +94,60 @@ while(run):
             print "\nMENTIONED PERSONALITIES:\n"
             for persona in persDict[news]:
                 print persona
-    elif ("5" in var):
-        print "5"
-    elif ("6" in var):
+    elif "5" in var:
+        print "\n"
+        query = raw_input("Enter a query to search: ")
+        extraction = Extractionpersonalities.Extraction(indexPath, persListpath)
+        persDict = extraction.seachPersonalities(query)
+        sentiment = SentimentAnalysis.SentimentA(stopwordList, vocabPolDict)
+        for news in persDict:
+            print "\nNEWS:\n"
+            n=news.split("::")
+            print "Title: " + n[0]
+            print "Summary:" + n[1]
+            print "\nMENTIONED PERSONALITIES:\n"
+            for persona in persDict[news]:
+                print persona
+            print '\nANALISE DE SENTIMENTOS:\n'
+            sentim = sentiment.sentiment(news, persDict[news])
+            if sentim==0:
+                print "neutro"
+            elif sentim>0:
+                print "positivo"
+            else:
+                print "negativo" 
+    elif "6" in var:
+        print "\n"
+        persSentDict = dict()
+        query = raw_input("Enter a query to search: ")
+        extraction = Extractionpersonalities.Extraction(indexPath, persListpath)
+        persDict = extraction.seachPersonalities(query)
+        sentiment = SentimentAnalysis.SentimentA(stopwordList, vocabPolDict)
+        for news in persDict:
+            sentim = sentiment.sentiment(news, persDict[news])
+            for pers in persDict[news]:
+                if pers in persSentDict:
+                    persSentDict[pers][0]+=1
+                    if sentim==0:
+                        persSentDict[pers][3]+=1    
+                    elif sentim>0:
+                        persSentDict[pers][1]+=1 
+                    else:
+                        persSentDict[pers][2]+=1
+                else:
+                    if sentim==0:
+                        persSentDict[pers]=[1,0,0,1]    
+                    elif sentim>0:
+                        persSentDict[pers]=[1,1,0,0] 
+                    else:
+                        persSentDict[pers]=[1,0,1,0]
+        for persona in persSentDict:
+            print "\nPERSONALITY: " + persona
+            print "NUMBER OF OCCURRENCES: " + str(persSentDict[persona][0])
+            print "NUMBER OF POSITIVE SENTIMENT ANALYSIS: " + str(persSentDict[persona][1]) 
+            print "NUMBER OF NEGATIVE SENTIMENT ANALYSIS: " + str(persSentDict[persona][2])
+            print "NUMBER OF NEUTRAL SENTIMENT ANALYSIS: " + str(persSentDict[persona][3]) 
+    elif "7" in var:
         print "\n"
         print "Tank you for your participation"
         run = False
--- eadw/sentimentanalysis/SentimentAnalysis.py
+++ eadw/sentimentanalysis/SentimentAnalysis.py
@@ -8,17 +8,88 @@ for line in stopwords:
     for word in l:
         stopwordList.append(word)
         
-print stopwordList
+#print stopwordList
 
 class SentimentA():
     
-    def __init__(self, stopWordL, persNewsDict, vocabPolDict):
+    def __init__(self, stopWordL, vocabPolDict):
         self.stopWList = stopWordL
-        self.persNewsD = persNewsDict
         self.vocabPolD = vocabPolDict
         
-    def sentiment(self):
-        "asasdasd"
+    def sentiment(self, news, personalList):
+        valor2=0
+        i=0
+        j=0
+        listaSplit2=[]
+        avaliacao = dict()
+
+        titulo = news.split("::")[0]
+        summary = news.split("::")[1]
+        summary2=summary.split()
+
+        ##Tratamento dos nomes das personalidades, retirar do texto
+        for personalidade in personalList:
+        
+            listaSplit1=personalidade.split()
+            for nomeAnome in listaSplit1:
+                listaSplit2.append(nomeAnome)
+            
+        
+    
+        for summary3 in summary2:
+        #print summary3
+        
+            n1=summary3.split(',')[0]
+            n2=n1.split('.')[0]
+            n=n2.split('...')[0]
+            
+
+            if stopwordList.count(n)<1 and n not in listaSplit2:
+                if n=="n\xc3\xa3o" or n=="N\xc3\xa3o":
+                    i=1
+                else:
+                    if i==1 and j<2:
+                        if self.vocabPolD.has_key(n):
+                            pol=self.vocabPolD.get(n)
+                            if int(pol)>0:
+                                avaliacao[n]="-1"
+                                i=0
+                                j=0
+                            elif int(pol)<0:
+                                avaliacao[n]="1"
+                                i=0
+                                j=0
+                            elif int(pol)==0:
+                                j=j+1
+                                pol="0"
+                                avaliacao[n]=pol
+                    else:
+                        if i==1 and j>2:
+                            j=0
+                            i=0
+                            avaliacao["Nao"]="-1"
+                        elif self.vocabPolD.has_key(n):
+                            pol=self.vocabPolD.get(n)
+                            #print n
+                            avaliacao[n]=pol
+                            #print pol
+                        else:
+                            pol="0"
+                            
+                            avaliacao[n]=pol
+                            #print n
+                            #print pol
+        #else:
+            #print summary3
+            #print "palavra rejeitada"
+        avaliacaoFinal=avaliacao.values()
+        print avaliacaoFinal
+        for valor in avaliacaoFinal:
+            valor2=valor2+int(valor)
+        print valor2
+        return valor2
+        
+        
         
 
         
